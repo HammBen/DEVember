ba71751320c99be01a4d9694c194d4bbc0b0bfb4
Benjamin Hammond <benjaminhammond268@gmail.com>
2024-02-27 13:54:21 +0000

    Custom component added, Advent Calendar Complete

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, TextStyle, View, ViewStyle } from "react-native";
+import DayListItem from "./src/components/core/DayListItem";
 
 export default function App() {
+  const days = [...Array(24)].map((val, index) => index + 1);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={$container}>
+      <FlatList
+        data={days}
+        renderItem={({ item }) => <DayListItem day={item} />}
+        contentContainerStyle={$content}
+        columnWrapperStyle={$column}
+        numColumns={2}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const $container: ViewStyle = {
+  flex: 1,
+  backgroundColor: "#fff",
+};
+
+const $content: ViewStyle = {
+  gap: 10,
+  padding: 10,
+};
+
+const $column: ViewStyle = {
+  gap: 10,
+};
+
+const $box: ViewStyle = {
+  backgroundColor: "#F9EDE3",
+  flex: 1,
+  aspectRatio: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  borderWidth: 0.5,
+  borderColor: "#9B4521",
+  borderRadius: 20,
+};
+
+const $text: TextStyle = {
+  color: "#9B4521",
+  fontSize: 50,
+};
---
+++ src/components/core/DayListItem.tsx
@@ -0,0 +1,30 @@
+import { View, Text, ViewStyle, TextStyle } from "react-native";
+import React from "react";
+
+interface DayListItemProp {
+  day: number;
+}
+
+export default function DayListItem({ day }: DayListItemProp) {
+  return (
+    <View style={$box}>
+      <Text style={$text}>{day}</Text>
+    </View>
+  );
+}
+
+const $box: ViewStyle = {
+  backgroundColor: "#F9EDE3",
+  flex: 1,
+  aspectRatio: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  borderWidth: 0.5,
+  borderColor: "#9B4521",
+  borderRadius: 20,
+};
+
+const $text: TextStyle = {
+  color: "#9B4521",
+  fontSize: 50,
+};
